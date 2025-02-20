7e98da10d7bfe8347a1df68a52ef8fd5162ae64a
bhatt-nish-0 <bhattnish0@gmail.com>
2025-02-20 13:51:22 +0800

    d

---
+++ src/decision/making/DoWhileExample.java
@@ -0,0 +1,11 @@
+package decision.making;
+
+public class DoWhileExample {
+    public static void main(String[] args) {
+        int lizard = 0;
+        do {
+            lizard++;
+        } while (false);
+        System.out.println(lizard);
+    }
+}
