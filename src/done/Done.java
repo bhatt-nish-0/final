720fa8e80d41ed7e2cf5ff6606dd7372bf5da983
bhatt-nish-0 <bhattnish0@gmail.com>
2025-02-20 11:48:04 +0800

    a

--- src/done/Done.java
+++ src/done/Done.java
@@ -3,7 +3,7 @@ package done;
 public class Done {
     public static void main(String[] args) {
         System.out.println("89 pages done");
-        System.out.println("27 pages done");
+        System.out.println("31 pages done");
 
     }
 }
