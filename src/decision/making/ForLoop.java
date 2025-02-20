b1b50fb97d8fd878fd05256aa678d59822430a5c
bhatt-nish-0 <bhattnish0@gmail.com>
2025-02-20 13:56:56 +0800

    a

---
+++ src/decision/making/ForLoop.java
@@ -0,0 +1,19 @@
+package decision.making;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        for (int i =0; i<= 10; i++) {
+            System.out.println(i);
+        }
+
+        //System.out.println(i);
+        int  j;
+        for (j=0; j<10; j++) {
+            System.out.println(j);
+        }
+
+        System.out.println("---");
+        System.out.println(j);
+    }
+}
