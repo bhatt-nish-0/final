587f99c6880690d0dc7bc7a08eb6ec158177db40
bhatt-nish-0 <bhattnish0@gmail.com>
2025-02-20 14:40:01 +0800

    a

--- src/done/Done.java
+++ src/done/Done.java
@@ -3,10 +3,10 @@ package done;
 public class Done {
     public static void main(String[] args) {
         System.out.println("89 pages done");
-        System.out.println("54 pages done");
+        System.out.println("57 pages done");
 
         int x = 89;
-        int y = 54;
+        int y = 57;
 
         System.out.println("total done :" + (x+y));
 
