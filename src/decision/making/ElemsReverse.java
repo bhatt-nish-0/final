b0c33ceb04887aca3bba882792c442a3a73c8848
bhatt-nish-0 <bhattnish0@gmail.com>
2025-02-20 14:09:26 +0800

    z

---
+++ src/decision/making/ElemsReverse.java
@@ -0,0 +1,30 @@
+package decision.making;
+
+public class ElemsReverse {
+    public static void main(String[] args) {
+
+//        for (;;){
+//            System.out.println("hi");
+//        }
+
+        int x = 0;
+        for (long y = 0, z = 4; x < 5 && y < 10; x++, y++) {
+            System.out.println(y + " "); }
+        System.out.print(x + " ");
+
+        int x1 = 0;
+        for (x1=0 ; x1 < 5; x1++)
+            System.out.print(x1 + " ");
+
+
+        //for (long y = 0, int  z = 4; x < 5; x++) {
+          //initiazlization block variables must be of
+        //  same type
+        //}
+
+        //for (int i = 0; i < 10; i++) {// Infinite Loop
+        //    System.out.println("what");
+        //    i = 0;
+        //}
+    }
+}
