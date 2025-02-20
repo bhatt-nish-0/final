4e4caee4f39735e4560ee7023540b6962e070ee8
bhatt-nish-0 <bhattnish0@gmail.com>
2025-02-20 14:52:26 +0800

    a

--- src/done/Done.java
+++ src/done/Done.java
@@ -5,10 +5,10 @@ import java.util.Optional;
 public class Done {
     public static void main(String[] args) {
         System.out.println("89 pages done");
-        System.out.println("59 pages done");
+        System.out.println("61 pages done");
 
         int x = 89;
-        int y = 59;
+        int y = 61;
 
         System.out.println("total done :" + (x+y));
 
---
+++ src/methodsstud/MethodMain.java
@@ -0,0 +1,15 @@
+package methodsstud;
+
+public class MethodMain {
+    public static void main(String[] args) {
+
+    }
+
+    public final void nap(int minutes) throws InterruptedException {
+        // nap(int minutes) --> method signature
+    }
+
+    void x() {
+        //simplest method possible
+    }
+}
